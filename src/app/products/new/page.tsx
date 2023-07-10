a800231fcc4fcf861abc31b7458e0d17c166d221
EvansRobbie <evansitoden94@gmail.com>
2023-07-11 00:12:12 +0300

    new page typescript fix

--- src/app/products/new/page.tsx
+++ src/app/products/new/page.tsx
@@ -1,15 +1,18 @@
 "use client"
-import React, { FormEvent } from 'react'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
 
 const newProduct = () => {
+    const [title, setTitle] = useState<string>("")
+    const [desc, setDesc] = useState<string>("")
+    const [price, setPrice] = useState<number>(0)
     const onSubmit = (e:FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        const target = e.target as HTMLFormElement
-        const title = (target[0] as HTMLInputElement).value
-        const desc = (target[1] as HTMLInputElement).value
-        const price = (target[2] as HTMLInputElement).value
+        // const target = e.target as HTMLFormElement
+        // const title = (target[0] as HTMLInputElement).value
+        // const desc = (target[1] as HTMLInputElement).value
+        // const price = (target[2] as HTMLInputElement).value
 
-        target.reset()
+        // target.reset()
 
         console.log(title, desc, price)
 
@@ -18,11 +21,11 @@ const newProduct = () => {
     <form onSubmit={onSubmit} className='flex flex-col  max-w-3xl mx-auto'>
         <h2 className=''>New Product</h2>
         <label htmlFor="prodName">Product name</label>
-        <input id='prodName' type="text" placeholder='Product name' required/>
+        <input id='prodName' type="text" onChange={(e)=>setTitle(e.target.value)} placeholder='Product name' required/>
         <label htmlFor="prodDesc">Description</label>
-        <textarea id='prodDesc' className='h-20' placeholder='Product description' required/>
+        <textarea id='prodDesc' className='h-20' onChange={(e)=>setDesc(e.target.value)} placeholder='Product description' required/>
         <label htmlFor="prodPrice">Price (Ksh)</label>
-        <input id='prodPrice' type="number" placeholder='price' />
+        <input id='prodPrice' type="number" onChange={(e)=>setPrice(Number(e.target.value))} placeholder='price' />
         <div className='flex justify-end'>
 
             <button className='button max-w-max'>Add Product</button>
