385a98c3ff4fb8ddb0eab598e1217b81871f2544
EvansRobbie <evansitoden94@gmail.com>
2023-07-28 14:27:06 +0300

    fix hidration error

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,14 +3,43 @@ import Link from 'next/link'
 import React from 'react'
 import useSWR from 'swr'
 
+interface productProp{
+  _id:string
+  title:string
+  desc:string
+  price:number
+}
+
 const products = () => {
   const fetcher = (...args:any) => fetch(args).then((res) => res.json())
-  const {data, error} = useSWR('/api/products', fetcher)
-  console.log(data)
+  const {data, error, isLoading} = useSWR('/api/products', fetcher)
+  // console.log(data)
 
   return (
     <div>
       <Link href={'/products/new'} className='button'>Add new product</Link>
+      {
+        isLoading ? 'Loading' : (
+
+        <table>
+          <thead>
+            <tr>
+              <td>Product Name</td>
+              <td>Pr</td>
+            </tr>
+          </thead>
+          <tbody>
+            { data.map(({_id, price, title, desc}:productProp) =>(
+              <tr >
+                <td >{title}</td>
+                <td>{price}</td>
+              </tr>
+            )) }
+
+          </tbody>
+        </table>
+        )
+      }
     </div>
   )
 }
