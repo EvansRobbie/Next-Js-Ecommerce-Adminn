765046ef9375459991698be4ba0bca0e81c69688
EvansRobbie <evansitoden94@gmail.com>
2023-07-31 03:42:54 +0300

    add category to products page

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,149 +1,197 @@
-"use client"
-import { fetcher } from '@/components/Fetcher'
-import Spinner from '@/components/Spinner'
-import axios from 'axios'
-import Link from 'next/link'
-import { notFound } from 'next/navigation'
-import React, { FormEvent, useState } from 'react'
-import { ToastContainer, toast } from 'react-toastify'
-import useSWR from 'swr'
-import { withSwal } from 'react-sweetalert2';
+"use client";
+import { fetcher } from "@/components/Fetcher";
+import Spinner from "@/components/Spinner";
+import axios from "axios";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import React, { FormEvent, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import useSWR from "swr";
+import { withSwal } from "react-sweetalert2";
 
-interface categoryProp{
-    _id:string
-    category:string
-    parentCategory?:{
-        _id:string 
-        category:string
-    }
+interface categoryProp {
+  _id: string;
+  category: string;
+  parentCategory?: {
+    _id: string;
+    category: string;
+  };
 }
 interface editProp {
-    _id:string
+  _id: string;
 }
 
-export default withSwal(({swal}:any, ref:any) => {
-    const [category, setCategory] = useState<string>('')
-    const [parentCategory, setParentCategory] = useState<string | undefined>('')
-    const [editCategory, setEditCategory] = useState<editProp | any>("")
-    const {data, error, isLoading, mutate} = useSWR('/api/categories', fetcher )
+export default withSwal(({ swal }: any, ref: any) => {
+  const [category, setCategory] = useState<string>("");
+  const [parentCategory, setParentCategory] = useState<string | null>(null);
+  const [editCategory, setEditCategory] = useState<editProp | any>("");
+  const { data, error, isLoading, mutate } = useSWR("/api/categories", fetcher);
 
-    if (error){
-        return notFound
-    }
-    // console.log(data)
-    const editCategoryName = (cat:categoryProp) =>{
-        setEditCategory(cat)
-        setCategory(cat.category)
-        setParentCategory(cat.parentCategory?._id)
-    }
+  if (error) {
+    return notFound;
+  }
+  // console.log(data)
+  const editCategoryName = (cat: categoryProp) => {
+    setEditCategory(cat);
+    setCategory(cat.category);
+    setParentCategory(cat.parentCategory?._id || null);
+  };
 
-    const deleteCategory = (cat:categoryProp) =>{
-        const {_id:id} = cat
-        swal.fire({
-            title: 'Are You sure?',
-            text: `Do you want to delete ${cat.category}?`,
-            showCancelButton: true,
-            cancelButtonText: "Cancel",
-            confirmButtonText: 'Yes, Delete',
-            confirmButtonColor: '#d55',
-            reverseButtons:true,
-        }).then(async ({isConfirmed}:{isConfirmed:boolean}) => {
-            if (isConfirmed){
-                await axios.delete(`/api/categories/${id}`)
-                mutate()
-                toast.success('Category Deleted')
-            }
-        }).catch((error:any) => {
-            console.log(error)
-           toast.error('Failed to delete Category')
-        });
-
-    }
-    const onSubmit = async (e:FormEvent<HTMLFormElement>) =>{
-        e.preventDefault()
-        const data = { category, parentCategory}
-        try {
-            if (editCategory){
-                // console.log(editCategory)
-                await axios.put('/api/categories', JSON.stringify({...data, _id:editCategory._id})) 
-                setEditCategory('')
-                toast.success('category editted')
-            }else{
-
-                await axios.post('/api/categories', JSON.stringify(data))
-                toast.success('category added')
-            }
-            setCategory('')
-            setParentCategory('')
-            mutate()
-            
-        } catch (error) {
-            console.log("Failed to save category", error)
-            toast.error('Failed to save category')
+  const deleteCategory = (cat: categoryProp) => {
+    const { _id: id } = cat;
+    swal
+      .fire({
+        title: "Are You sure?",
+        text: `Do you want to delete ${cat.category}?`,
+        showCancelButton: true,
+        cancelButtonText: "Cancel",
+        confirmButtonText: "Yes, Delete",
+        confirmButtonColor: "#d55",
+        reverseButtons: true,
+      })
+      .then(async ({ isConfirmed }: { isConfirmed: boolean }) => {
+        if (isConfirmed) {
+          await axios.delete(`/api/categories/${id}`);
+          mutate();
+          toast.success("Category Deleted");
         }
+      })
+      .catch((error: any) => {
+        console.log(error);
+        toast.error("Failed to delete Category");
+      });
+  };
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = { category, parentCategory:parentCategory || null };
+    try {
+      if (editCategory) {
+        // console.log(editCategory)
+        await axios.put(
+          "/api/categories",
+          JSON.stringify({ ...data, _id: editCategory._id })
+        );
+        setEditCategory("");
+        toast.success("category editted");
+      } else {
+        await axios.post("/api/categories", JSON.stringify(data));
+        toast.success("category added");
+      }
+      setCategory("");
+      setParentCategory("");
+      mutate();
+    } catch (error) {
+      console.log("Failed to save category", error);
+      toast.error("Failed to save category");
     }
+  };
 
-   
-    // console.log(editCategory)
+  // console.log(editCategory)
   return (
-    <div className=' max-w-3xl mx-auto'>
-         {isLoading ? <div className='flex w-full justify-center h-[50vh] items-center'><Spinner/></div>: <>
-        <form onSubmit={onSubmit} className='flex flex-col ' >
+    <div className=" max-w-3xl mx-auto">
+      {isLoading ? (
+        <div className="flex w-full justify-center h-[50vh] items-center">
+          <Spinner />
+        </div>
+      ) : (
+        <>
+          <form onSubmit={onSubmit} className="flex flex-col ">
             <h2>Categories</h2>
-                <label htmlFor="category">{editCategory ? `Edit category ${editCategory.category}` :"Create New Category Name" }</label>
-                <div className='flex gap-1 items-center py-2'>
-                    <input type="text" id="category" value={category} onChange={(e)=>setCategory(e.target.value)} placeholder='Category Name'/>
-                    <select className='bg-slate-200 font-semibold text-slate-900 ' value={parentCategory} onChange={(e)=>setParentCategory(e.target.value)} >
-                        <option className=''  value="">No Parent Category</option>
-                       {data.map(({category, _id}: categoryProp)=>(
-                            <option className='!bg-slate-200/10 ' key={_id} value={_id}>{category}</option>
-                        ))}
-                    </select>
-                    <button className='button max-w-max -mt-3.5 '>Save</button>
-                </div>
-                <ToastContainer/>
-        </form>
+            <label htmlFor="category">
+              {editCategory
+                ? `Edit category ${editCategory.category}`
+                : "Create New Category Name"}
+            </label>
+            <div className="flex gap-1 items-center py-2">
+              <input
+                type="text"
+                id="category"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+                placeholder="Category Name"
+              />
+              <select
+                className="bg-slate-200 font-semibold text-slate-900 "
+                value={parentCategory || ''}
+                onChange={(e) => setParentCategory(e.target.value)}
+              >
+                <option className="" value="">
+                  No Parent Category
+                </option>
+                {data.map(({ category, _id }: categoryProp) => (
+                  <option className="!bg-slate-200/10 " key={_id} value={_id}>
+                    {category}
+                  </option>
+                ))}
+              </select>
+              <button className="button max-w-max -mt-3.5 ">Save</button>
+            </div>
+            <ToastContainer />
+          </form>
 
-        <table>
+          <table>
             <thead>
-                <tr>
-                    <td>Category Name</td>
-                    <td>Parent Category</td>
-                    <td></td>
-                </tr>
+              <tr>
+                <td>Category Name</td>
+                <td>Parent Category</td>
+                <td></td>
+              </tr>
             </thead>
             <tbody>
-                { data.map((cat: categoryProp)=>(
+              {data.map((cat: categoryProp) => (
                 <tr key={cat._id}>
-                        <td > {cat.category}</td>
-                        <td>{cat?.parentCategory?.category}</td>
-                        <td className=' items-center flex justify-end gap-4'>
-                  <button onClick={() => editCategoryName(cat)} className='bg-orange-500 py-1 px-2.5 gap-1 inline-flex rounded-md'>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                </svg>
-                    <span>Edit</span>
+                  <td> {cat.category}</td>
+                  <td>{cat?.parentCategory?.category}</td>
+                  <td className=" items-center flex justify-end gap-4">
+                    <button
+                      onClick={() => editCategoryName(cat)}
+                      className="bg-orange-500 py-1 px-2.5 gap-1 inline-flex rounded-md"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-5 h-5"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                        />
+                      </svg>
+                      <span>Edit</span>
                     </button>
 
-                    <button onClick={() =>deleteCategory(cat)} className='bg-red-500/80 py-1 px-2.5 gap-1 inline-flex rounded-md'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                    <button
+                      onClick={() => deleteCategory(cat)}
+                      className="bg-red-500/80 py-1 px-2.5 gap-1 inline-flex rounded-md"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-5 h-5"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
                       </svg>
 
-                    <span>Delete</span>
+                      <span>Delete</span>
                     </button>
-                </td>
+                  </td>
                 </tr>
-                    )
-                )}
-                    
+              ))}
             </tbody>
-
-        </table>
+          </table>
         </>
-}
-
-
+      )}
     </div>
-  )
-})
+  );
+});
--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -1,15 +1,25 @@
 "use client";
 import axios from "axios";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { notFound, useRouter } from "next/navigation";
 import React, { ChangeEvent, FormEvent, useState } from "react";
 import Spinner from "./Spinner";
 import { ReactSortable } from "react-sortablejs";
+import { fetcher } from "./Fetcher";
+import useSWR from 'swr'
 
 interface ImageFile extends File {
   id: number;
 }
 
+interface categoryProp {
+  _id: string;
+  category: string;
+  parentCategory?: {
+    _id: string;
+    category: string;
+  };
+}
 const ProductForm = ({
   _id: id,
   title: productTitle,
@@ -29,7 +39,12 @@ const ProductForm = ({
   const [desc, setDesc] = useState<string>(productDesc || "");
   const [price, setPrice] = useState<number>(productPrice || 0);
   const [isUpLoading, setIsUploading] = useState<boolean>(false);
+  const { data:categories, error, isLoading } = useSWR("/api/categories", fetcher);
   const router = useRouter();
+
+  if (error){
+    return notFound
+  }
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const target = e.target as HTMLFormElement;
@@ -88,93 +103,109 @@ const ProductForm = ({
   //   setImage([...image.filter((photo)=> photo !== link)])
   // }
   return (
+    
     <form onSubmit={onSubmit} className="flex flex-col  max-w-3xl mx-auto">
-      <h2 className="">{id ? "Edit" : "New"} Product</h2>
-      <label htmlFor="prodName">Product name</label>
-      <input
-        id="prodName"
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        placeholder="Product name"
-        required
-      />
-      <label htmlFor="photo">Image</label>
-      <div className="flex  gap-1  items-center">
-        <ReactSortable
-          list={image as ImageFile[]}
-          setList={updateImagesOrder}
-          className="flex gap-2 relative"
-        >
-          {!!image.length &&
-            image.map((link, idx) => (
-              <>
-                <div key={idx} className="relative h-24 w-28 mb-4">
-                  <Image
-                    fill={true}
-                    className="max-h-full object-cover rounded-xl"
-                    src={`${link}`}
-                    alt={`/product/${link}`}
-                  />
-                </div>
-                {/* <button onClick={(e) =>removeImage(e,link)} className='absolute top-1 right-1 bg-black cursor-pointer text-white py-1.5 px-2 md:py-2 md:px-3 rounded-xl bg-opacity-50'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 ">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-              </svg>
-
-            </button> */}
-              </>
-            ))}
-        </ReactSortable>
-        {isUpLoading && (
-          <div className="h-24 flex items-center">
-            <Spinner />
-          </div>
-        )}
-        <div className="mb-2">
-          <label className="w-24 h-24 cursor-pointer text-gray-300/50 rounded-lg text-sm  bg-slate-200/10 flex flex-col items-center justify-center gap-1">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M7.5 7.5h-.75A2.25 2.25 0 004.5 9.75v7.5a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25v-7.5a2.25 2.25 0 00-2.25-2.25h-.75m0-3l-3-3m0 0l-3 3m3-3v11.25m6-2.25h.75a2.25 2.25 0 012.25 2.25v7.5a2.25 2.25 0 01-2.25 2.25h-7.5a2.25 2.25 0 01-2.25-2.25v-.75"
-              />
-            </svg>
-            <span>Upload</span>
-            <input type="file" className="hidden" onChange={uploadImages} />
-          </label>
-          {!image && (
-            <div className="text-gray-300/50">No Images in this product</div>
-          )}
+      {isLoading ? (
+        <div className="flex w-full justify-center h-[50vh] items-center">
+          <Spinner />
         </div>
-      </div>
-      <label htmlFor="prodDesc">Description</label>
-      <textarea
-        id="prodDesc"
-        className="h-20"
-        value={desc}
-        onChange={(e) => setDesc(e.target.value)}
-        placeholder="Product description"
-        required
-      />
-      <label htmlFor="prodPrice">Price (Ksh)</label>
-      <input
-        id="prodPrice"
-        type="number"
-        value={price}
-        onChange={(e) => setPrice(Number(e.target.value))}
-        placeholder="price"
-      />
-      <div className="flex justify-end">
-        <button className="button max-w-max">Save Product</button>
-      </div>
+      ) :(
+          <>
+            <h2 className="">{id ? "Edit" : "New"} Product</h2>
+            <label htmlFor="prodName">Product name</label>
+            <input
+              id="prodName"
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Product name"
+              required
+            />
+            <label htmlFor="category">Category</label>
+            <select className="text-slate-900 bg-slate-200 font-semibold" id="category">
+              <option value="">Uncategorized</option>
+                {categories.map(({_id, category}:categoryProp) =>(
+                    <option key={_id} value={_id}>{category}</option>
+                ))}
+            </select>
+            <label htmlFor="photo">Image</label>
+            <div className="flex  gap-1  items-center">
+              <ReactSortable
+                list={image as ImageFile[]}
+                setList={updateImagesOrder}
+                className="flex gap-2 relative"
+              >
+                {!!image.length &&
+                  image.map((link, idx) => (
+                    <>
+                      <div key={idx} className="relative h-24 w-28 mb-4">
+                        <Image
+                          fill={true}
+                          className="max-h-full object-cover rounded-xl"
+                          src={`${link}`}
+                          alt={`/product/${link}`}
+                        />
+                      </div>
+                      {/* <button onClick={(e) =>removeImage(e,link)} className='absolute top-1 right-1 bg-black cursor-pointer text-white py-1.5 px-2 md:py-2 md:px-3 rounded-xl bg-opacity-50'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 ">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                    </svg>
+
+                  </button> */}
+                    </>
+                  ))}
+              </ReactSortable>
+              {isUpLoading && (
+                <div className="h-24 flex items-center">
+                  <Spinner />
+                </div>
+              )}
+              <div className="mb-2">
+                <label className="w-24 h-24 cursor-pointer text-gray-300/50 rounded-lg text-sm  bg-slate-200/10 flex flex-col items-center justify-center gap-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M7.5 7.5h-.75A2.25 2.25 0 004.5 9.75v7.5a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25v-7.5a2.25 2.25 0 00-2.25-2.25h-.75m0-3l-3-3m0 0l-3 3m3-3v11.25m6-2.25h.75a2.25 2.25 0 012.25 2.25v7.5a2.25 2.25 0 01-2.25 2.25h-7.5a2.25 2.25 0 01-2.25-2.25v-.75"
+                    />
+                  </svg>
+                  <span>Upload</span>
+                  <input type="file" className="hidden" onChange={uploadImages} />
+                </label>
+                {!image && (
+                  <div className="text-gray-300/50">No Images in this product</div>
+                )}
+              </div>
+            </div>
+            <label htmlFor="prodDesc">Description</label>
+            <textarea
+              id="prodDesc"
+              className="h-20"
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
+              placeholder="Product description"
+              required
+            />
+            <label htmlFor="prodPrice">Price (Ksh)</label>
+            <input
+              id="prodPrice"
+              type="number"
+              value={price}
+              onChange={(e) => setPrice(Number(e.target.value))}
+              placeholder="price"
+            />
+            <div className="flex justify-end">
+              <button className="button max-w-max">Save Product</button>
+            </div>
+          </>
+      )}
     </form>
   );
 };
