59359c0edc3a7b5a16ccfc2de66dbf5cbf782bff
EvansRobbie <evansitoden94@gmail.com>
2023-08-12 15:54:58 +0300

    add spinnner gif to loading

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -2,13 +2,16 @@
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import React from 'react'
+import Spinner from './Spinner'
 
 const ProtectedRoute = ({children}:{children:React.ReactNode}) => {
     const { data:session, status, } = useSession()
     // console.log(session?.user?.image)
     const router =  useRouter()
-    if (status === 'loading'){
-      return <p>Loading</p>
+    if (status === "loading"){
+      return <div className="flex w-full justify-center h-[50vh] items-center">
+      <Spinner />
+    </div>
     }
     if (status === 'unauthenticated'){
       router.push('/login')
